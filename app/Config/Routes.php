b4ffa18ab160a4c81792e4f5eb9ce98292910353
devi <vdevi5117@gmail.com>
2025-11-26 12:29:02 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/home', 'Home::index');
 $routes->get('/login', 'Login::index');
 $routes->post('/login/auth', 'Login::auth');
 $routes->get('/register', 'Register::index');
